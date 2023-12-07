99ea30486a0ef95437e1508cc12558331f41ace9
imkzuma <edu.gungkrisna@gmail.com>
2023-12-08 00:17:57 +0800

    (feat): Create modules to handle delete file in bucket

---
+++ modules/DeleteBucketFile.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const path = require('path');
+const { Storage } = require('@google-cloud/storage');
+
+const pathKey = path.resolve('./serviceaccount.json');
+
+const gcs = new Storage({
+  projectId: process.env.projectId,
+  keyFilename: pathKey
+});
+
+const deleteFromGcs = async (fileName) => {
+    const bucketName = process.env.bucketName;
+    const bucket = gcs.bucket(bucketName);
+    const file = bucket.file(fileName);
+  
+    await file.delete();
+}
+
+module.exports = deleteFromGcs;
